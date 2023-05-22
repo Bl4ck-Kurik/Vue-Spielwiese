55f55d38cc0373907ac78c3f753f096246ba4810
Bl4ck-Kurik <123986218+Bl4ck-Kurik@users.noreply.github.com>
2023-05-22 14:09:19 +0200

    Commit

---
+++ src/components/TaskInput.vue
@@ -0,0 +1,28 @@
+<template lang="">
+    <div>
+        <input v-model="newTaskText" type="text" placeholder="Hier Text Eingeben!">
+        <button @click="addTask">Hinzufügen</button>        
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            newTaskText: ''
+        }
+    },
+    methods: {
+        addTask () {
+            const newtask = {
+                text: this.newTaskText,
+                completet: false
+            };
+            this.$emit('task-added', newTask);
+            this.newTaskText= ''
+        }
+    },
+}
+</script>
+<style lang="">
+    
+</style>
